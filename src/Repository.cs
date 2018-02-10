90717930cf440396f57e691a30fce0a3a978d99b
akeil <alex@akeil.net>
2018-02-10 11:55:24 +0100

    remove unused methods for account access (repo), do not cache account info, read json-file every time

--- src/Repository.cs
+++ src/Repository.cs
@@ -28,20 +28,7 @@ namespace ownzone
     // Holds zone definitions
     public interface IRepository
     {
-        // List all account names.
-        IEnumerable<string> GetAccountNames();
-
-        // Get an Account by name.
-        Account GetAccount(string name);
-
-        // Get the list of zones associated to the given account name.
-        IEnumerable<IZone> GetZones(string account);
-
-        Task<IEnumerable<string>> GetAccountNamesAsync();
-
-        Task<Account> GetAccountAsync(string name);
-
-        Task<IEnumerable<IZone>> GetZonesAsync(string name);
+        Task<IEnumerable<IZone>> GetZonesAsync(string userName);
     }
 
     // Configuration settings for the Zone Repository.
@@ -64,10 +51,6 @@ namespace ownzone
 
         private readonly RepoSettings settings;
 
-        private readonly Dictionary<string, Account> accounts;
-
-        private bool accountsRead;
-
         public Repository(ILoggerFactory loggerFactory,
             IConfiguration config)
         {
@@ -78,89 +61,18 @@ namespace ownzone
 
             log.LogInformation("Init Repository, basedir is {0}.",
                 settings.BaseDirectory);
-
-            accounts = new Dictionary<string, Account>();
-            accountsRead = false;
-        }
-
-        public IEnumerable<string> GetAccountNames()
-        {
-            var task = GetAccountNamesAsync();
-            task.Wait();
-            return task.Result;
-        }
-
-        public async Task<IEnumerable<string>> GetAccountNamesAsync()
-        {
-            await lazyReadAccountsAsync();
-            return accounts.Keys;
-        }
-
-        public Account GetAccount(string name)
-        {
-            var task = GetAccountAsync(name);
-            task.Wait();
-            return task.Result;
-        }
-
-        public async Task<Account> GetAccountAsync(string name)
-        {
-            await lazyReadAccountsAsync();
-            return accounts[name];
-        }
-
-        public IEnumerable<IZone> GetZones(string name)
-        {
-            var task = GetZonesAsync(name);
-            task.Wait();
-            return task.Result;
         }
 
         public async Task<IEnumerable<IZone>> GetZonesAsync(string name)
         {
-            await lazyReadAccountsAsync();
-            var account = await GetAccountAsync(name);
+            var account = await readAccountAsync(name);
             return account.GetZones();
         }
 
-        private async Task lazyReadAccountsAsync()
+        private async Task<Account> readAccountAsync(string name)
         {
-            if (!accountsRead)
-            {
-                try
-                {
-                    await readAccountsAsync();
-                }
-                catch(Exception ex)
-                {
-                    log.LogError(ex, "Error reading account");
-                }
-                accountsRead = true;
-            }
-        }
-
-        private async Task readAccountsAsync()
-        {
-            var path = settings.BaseDirectory;
-            foreach (var filename in Directory.EnumerateFiles(path, "*.json"))
-            {
-                try
-                {
-                    var account = await readAccountAsync(filename);
-                    accounts[account.Name] = account;
-                }
-                catch (AccountReadException ex)
-                {
-                    // skip invalid account definitions
-                    log.LogError(ex, "Failed to load account from {0}.",
-                        filename);
-                }
-            }
-        }
-
-        private async Task<Account> readAccountAsync(string path)
-        {
-            log.LogInformation("Read account info from {0}.", path);
+            var path = Path.Combine(settings.BaseDirectory, name + ".json");
+            log.LogInformation("Read account {0} from {1}.", name, path);
 
             var json = "";
             using (StreamReader reader = new StreamReader(path, Encoding.UTF8))
@@ -169,9 +81,7 @@ namespace ownzone
             }
             var account = JsonConvert.DeserializeObject<Account>(json);
 
-            var name = Path.GetFileNameWithoutExtension(path);
             account.Name = name;
-
             return account;
         }
     }
